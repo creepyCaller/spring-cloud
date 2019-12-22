63edf92097d832e3ba4b20b1aef01d682856e8d3
creepyCaller <fangpengcourage@outlook.com>
2019-12-22 16:13:05 +0800

    Spring Cloud Eureka Server 注册中心框架搭建

---
+++ eurekaserver/src/main/java/cn/edu/cuit/springcloud/EurekaServerApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.cuit.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication //声明该类为Spring-Boot服务的入口
+@EnableEurekaServer //声明该类是一个Eureka-Server微服务，提供服务注册和服务发现功能（即注册中心）
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
